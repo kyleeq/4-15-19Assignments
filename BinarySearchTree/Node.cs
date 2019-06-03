f9166c51da71ed129056c1412b3fa36f9c9d679d
unknown <kylee.schreibs2@yahoo.com>
2019-06-03 13:21:23 -0500

    add node class

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public Node RightNode { get; set; }
+        public Node LeftNode { get; set; }
+        public int Value { get; set; }
+
+    }
+}
